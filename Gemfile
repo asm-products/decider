1395960a245fe17069ac9d34da8a0703383ba4f2
eahanson <erik@eahanson.com>
2014-09-07 22:59:11 -0700

    basic rails

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.1.5'
+gem 'sass-rails', '~> 4.0.3'
+gem 'spring',        group: :development
+gem 'therubyracer',  platforms: :ruby
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo '>>> DISABLE STRICT HOST KEY CHECKING FOR SSH CONNECTIONS TO GITHUB'
+echo ">>> DISABLE STRICT HOST KEY CHECKING FOR SSH CONNECTIONS TO GITHUB"
 touch /home/vagrant/.ssh/config
 cat >> /home/vagrant/.ssh/config <<"EOF"
 Host github.com
@@ -9,7 +9,7 @@ Host github.com
 EOF
 
 if [ ! -f /home/vagrant/.rbenv ]; then
-  echo '>>> INSTALL RBENV'
+  echo ">>> INSTALL RBENV"
   git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
   touch /home/vagrant/.bash_profile
   echo 'export PATH="/home/vagrant/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
@@ -23,5 +23,14 @@ echo ">>> INSTALL RUBY VIA RBENV"
 source /home/vagrant/.bash_profile
 cd /vagrant && rbenv install && rbenv rehash
 
+echo ">>> GEMRC"
+touch /home/vagrant/.gemrc
+echo "gem: --no-document" >> /home/vagrant/.gemrc
+
 echo ">>> INSTALL BUNDLER"
 cd /vagrant && gem install bundler
+rbenv rehash
+
+echo ">>> BUNDLE"
+cd /vagrant && bundle
+
