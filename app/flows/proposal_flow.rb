2422c1a99ce23261f73978e35ee8799ff81a15b9
Erik Hanson & Noah Thorp <pair+erik+noah@citizencode.io>
2014-09-09 12:06:27 -0700

    Send all information to the proposal mailer

--- app/flows/proposal_flow.rb
+++ app/flows/proposal_flow.rb
@@ -1,12 +1,17 @@
 class ProposalFlow
   def create_proposal(description:, proposer:, proposer_email:, stakeholder_emails:)
     proposal = Proposal.create!(description: description, proposer: proposer)
-    stakeholder_emails = parse_emails(stakeholder_emails) << proposer_email
-    (stakeholder_emails).each do |email|
+    stakeholder_emails = parse_emails(stakeholder_emails, proposer_email)
+
+    stakeholder_emails.each do |email|
       proposal.stakeholders.create!(email: email)
     end
 
-    ProposingMailer.propose(recipients: stakeholder_emails, subject: '', proposal: '').deliver
+    ProposingMailer.propose(
+      recipients: stakeholder_emails,
+      subject: "New proposal from #{proposer}",
+      proposal: description
+    ).deliver
   end
 
   def proposals
@@ -21,9 +26,10 @@ class ProposalFlow
 
   private
 
-  def parse_emails(email_string)
-    email_string.strip!
-    email_string.split /[\s,]+/
+  def parse_emails(*email_strings)
+    email_strings.each_with_object([]) do |email_string, emails|
+      emails.concat email_string.strip.split /[\s,]+/
+    end
   end
 end
 
--- spec/flows/proposal_flow_spec.rb
+++ spec/flows/proposal_flow_spec.rb
@@ -1,43 +1,56 @@
 require "rails_helper"
 
 describe ProposalFlow do
+  let(:tolkein) { 'jrr.tolkein@example.com' }
+  let(:martin) { 'grr.martin@example.com' }
+  let(:rowling) { 'jk.rowling@example.com' }
+  let(:gaiman) { 'n.gaiman@example.com' }
+
   describe '#create_proposal' do
-    let(:new_proposal_attributes) do
-      { proposer: 'J.R.R. Tolkein',
-        proposer_email: 'jrr@example.com',
+    let(:flow) { ProposalFlow.new }
+    let(:last_proposal) { Proposal.last }
+    let(:last_email) { ActionMailer::Base.deliveries.last }
+
+    before do
+      flow.create_proposal(
+        proposer: 'J.R.R. Tolkein',
+        proposer_email: tolkein,
         description: 'new elf world',
-        stakeholder_emails: 'dadams@example.com oscard@example.com lalexander@example.com' }
+        stakeholder_emails: [martin, rowling, gaiman].join(' ')
+      )
     end
 
-    let(:flow) { ProposalFlow.new }
+    describe 'creating a proposal record' do
+      specify { expect(last_proposal.proposer).to eq 'J.R.R. Tolkein' }
+      specify { expect(last_proposal.description).to eq 'new elf world' }
+      specify { expect(last_proposal.stakeholders.pluck(:email)).to match_array [tolkein, martin, rowling, gaiman] }
+    end
 
-    before { flow.create_proposal(new_proposal_attributes) }
-
-    specify { expect(Proposal.pluck(:proposer, :description).last).to eq ['J.R.R. Tolkein', 'new elf world'] }
-    specify do
-      expect(Proposal.last.stakeholders.pluck(:email)).
-      to match_array %w[dadams@example.com jrr@example.com oscard@example.com lalexander@example.com]
+    describe 'sending email' do
+      specify { expect(last_email.subject).to eq 'New proposal from J.R.R. Tolkein' }
+      specify { expect(last_email.to).to match_array [tolkein, martin, rowling, gaiman] }
+      specify { expect(last_email.body).to match 'new elf world' }
     end
   end
 
   describe '#proposals' do
     let!(:proposal1) do
       Proposal.create!(proposer: 'J.K. Rowling', description: 'more spells').tap do |proposal|
-        proposal.stakeholders.create! email: 'dadams@example.com'
-        proposal.stakeholders.create! email: 'oscard@example.com'
+        proposal.stakeholders.create! email: martin
+        proposal.stakeholders.create! email: rowling
       end
     end
 
     let!(:proposal2) do
       Proposal.create!(proposer: 'J.R.R. Tolkein', description: 'new elf world').tap do |proposal|
-        proposal.stakeholders.create! email: 'lalexander@example.com'
+        proposal.stakeholders.create! email: gaiman
       end
     end
 
     specify do
       expect(ProposalFlow.new.proposals).to match_array([
-        { proposer: 'J.K. Rowling', description: 'more spells', stakeholder_emails: ['dadams@example.com', 'oscard@example.com'] },
-        { proposer: 'J.R.R. Tolkein', description: 'new elf world', stakeholder_emails: ['lalexander@example.com'] }
+        { proposer: 'J.K. Rowling', description: 'more spells', stakeholder_emails: [martin, rowling] },
+        { proposer: 'J.R.R. Tolkein', description: 'new elf world', stakeholder_emails: [gaiman] }
       ])
     end
   end
@@ -47,5 +60,6 @@ describe ProposalFlow do
     specify { expect(flow.send(:parse_emails, 'a@example.com b@example.com')).to eq ['a@example.com', 'b@example.com'] }
     specify { expect(flow.send(:parse_emails, 'a@example.com, b@example.com')).to eq ['a@example.com', 'b@example.com'] }
     specify { expect(flow.send(:parse_emails, "  a@example.com   , \t \nb@example.com")).to eq ['a@example.com', 'b@example.com'] }
+    specify { expect(flow.send(:parse_emails, " a@example.com ", "  b@example.com \nc@example.com")).to eq ['a@example.com', 'b@example.com', 'c@example.com'] }
   end
 end
